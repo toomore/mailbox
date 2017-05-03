b31cc689275f67c082868ac154f8200a1da1a190
Toomore Chiang <toomore0929@gmail.com>
2017-05-03 08:26:11 +0000

    Init

---
+++ run-mariadb-client.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+docker run -it --rm --link mailbox-mariadb:MARIADB               \
+           -v $(pwd)/sql:/sql                                    \
+           -v $(pwd)/mariadb.cnf:/etc/mysql/conf.d/mariadb.cnf   \
+           mariadb:10.1.22 bash
