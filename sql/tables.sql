75da759c20e2cc50e4d71db561a37fbfbc995142
Toomore Chiang <toomore0929@gmail.com>
2017-05-03 10:31:25 +0000

    Add user table

--- sql/tables.sql
+++ sql/tables.sql
@@ -6,3 +6,17 @@ CREATE TABLE IF NOT EXISTS campaign (
     ) CHARACTER SET 'utf8' 
       COLLATE 'utf8_icelandic_ci';
 
+CREATE TABLE IF NOT EXISTS user (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(64) NOT NULL,
+    groups VARCHAR(64) NOT NULL,
+    f_name VARCHAR(32) NOT NULL,
+    l_name VARCHAR(32) not NULL,
+    created DATETIME DEFAULT NOW(),
+    updated DATETIME DEFAULT NOW() ON UPDATE NOW(),
+    alive BOOL DEFAULT true,
+    PRIMARY KEY (id),
+    UNIQUE KEY email_groups (email, groups),
+    INDEX groups (groups ASC)
+    ) CHARACTER SET 'utf8'
+      COLLATE 'utf8_icelandic_ci';
