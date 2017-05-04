56ede6ceb2cb96cef1a4cfa4de113b4ee1c0762e
Toomore Chiang <toomore0929@gmail.com>
2017-05-04 06:04:57 +0000

    Add GenSeed

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,20 @@
 package utils
 
+import (
+	"encoding/hex"
+
+	"github.com/google/uuid"
+)
+
 const (
 	// SQLPATH is for SQL link path
 	SQLPATH = "root:mailboxdbs@tcp(MARIADB:3306)/mailbox?parseTime=true"
 )
+
+// GenSeed is to gen seed
+func GenSeed() [8]byte {
+	var buf [8]byte
+	u := uuid.Must(uuid.NewRandom())
+	hex.Encode(buf[:], u[:4])
+	return buf
+}
