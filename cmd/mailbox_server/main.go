5ebb5aa64eed8e79094cdb104fe869350a37ce08
Toomore Chiang <toomore0929@gmail.com>
2017-05-06 06:38:33 +0000

    Using nginx proxy set header

--- cmd/mailbox_server/main.go
+++ cmd/mailbox_server/main.go
@@ -12,15 +12,16 @@ import (
 	"github.com/toomore/mailbox/campaign"
 )
 
-var httpPort = flag.String("p", ":8080", "Http port")
+var httpPort = flag.String("p", ":8801", "Http port")
 var readExpr = regexp.MustCompile(`/read/([0-9a-z]+)`)
 
 func read(w http.ResponseWriter, r *http.Request) {
-	u, _ := url.Parse(r.RequestURI)
-	v, _ := url.ParseQuery(u.RawQuery)
+	//u, _ := url.Parse(r.RequestURI)
+	//v, _ := url.ParseQuery(u.RawQuery)
+	v, _ := url.ParseQuery(r.Header.Get("X-Args"))
 
 	var hm string
-	match := readExpr.FindStringSubmatch(u.Path)
+	match := readExpr.FindStringSubmatch(r.Header.Get("X-Uri"))
 	if len(match) > 1 {
 		hm = match[1]
 	}
@@ -31,6 +32,9 @@ func read(w http.ResponseWriter, r *http.Request) {
 	} else {
 		log.Println("Hash Fail!!!")
 	}
+	log.Printf("%+v", r)
+	log.Println(r.Header.Get("X-Real-Ip"))
+	log.Println(r.Header.Get("User-Agent"))
 }
 
 func main() {
--- dev-run-docker.sh
+++ dev-run-docker.sh
@@ -3,6 +3,6 @@ docker run -it --rm  --link mailbox-mariadb:MARIADB              \
            -v $(pwd)/cmd:/cmd                                    \
            -v $(pwd)/campaign:/go/src/github.com/toomore/mailbox/campaign      \
            -v $(pwd)/utils:/go/src/github.com/toomore/mailbox/utils            \
-           -p 127.0.0.1:5590:5590                                              \
+           -p 127.0.0.1:8801:8801                                              \
            golang:1.8.1 bash
 
