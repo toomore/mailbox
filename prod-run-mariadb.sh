0c6c8dbed3c9c8eca8e4a5de077762f7963efc3d
Toomore Chiang <toomore0929@gmail.com>
2017-05-07 05:48:02 +0000

    Add prod mariadb

---
+++ prod-run-mariadb.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+docker run -d --name mailbox-mariadb-prod                                   \
+           -v /srv/mailbox_mariadb_prod:/var/lib/mysql                      \
+           --log-opt max-size=64m                                           \
+           --log-opt max-file=1                                             \
+           -e MYSQL_ROOT_PASSWORD=mailboxdbs                                \
+           -e CHARACTER_SET_SERVER='utf8'                                   \
+           -e COLLATION_SERVER='utf8_general_ci'                            \
+           -e INIT_CONNECT='SET NAMES utf8'                                 \
+           mariadb:10.1.22
