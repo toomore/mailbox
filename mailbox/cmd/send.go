2059589eeb13d076a774f6bf4103810360c3b2e5
Toomore Chiang <toomore0929@gmail.com>
2017-06-28 08:32:26 +0000

    Add send

---
+++ mailbox/cmd/send.go
@@ -0,0 +1,89 @@
+// Copyright © 2017 Toomore Chiang
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/toomore/mailbox/mails"
+	"github.com/toomore/mailbox/utils"
+)
+
+var (
+	sendCID         *string
+	sendUID         *string
+	sendDryRun      *bool
+	sendGroups      *string
+	sendPath        *string
+	sendReplaceLink *bool
+	sendSubject     *string
+)
+
+// sendCmd represents the send command
+var sendCmd = &cobra.Command{
+	Use:   "send",
+	Short: "send paper",
+	Long:  `send paper`,
+	Run: func(cmd *cobra.Command, args []string) {
+		file, err := os.Open(*sendPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		body, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var rows *sql.Rows
+		if *sendUID != "" {
+			uids := strings.Split(*sendUID, ",")
+			for i, v := range uids {
+				uids[i] = fmt.Sprintf("'%s'", v)
+			}
+			rows, err = utils.GetConn().Query(fmt.Sprintf(`SELECT id,email,f_name,l_name FROM user WHERE alive=1 AND id IN (%s)`, strings.Join(uids, ",")))
+		} else {
+			rows, err = utils.GetConn().Query(`SELECT id,email,f_name,l_name FROM user WHERE alive=1 AND groups=?`, *sendGroups)
+		}
+		defer rows.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		mails.ProcessSend(body, rows, *sendCID, *sendReplaceLink, *sendSubject, *sendDryRun)
+	},
+}
+
+func init() {
+	sendCID = sendCmd.Flags().String("cid", "", "Campaign ID")
+	sendUID = sendCmd.Flags().String("uid", "", "User ID, support more by splited with ','")
+	sendDryRun = sendCmd.Flags().BoolP("dryrun", "d", false, "Dry run")
+	sendGroups = sendCmd.Flags().StringP("groups", "g", "", "User groups")
+	sendPath = sendCmd.Flags().StringP("path", "p", "", "HTML file path")
+	sendReplaceLink = sendCmd.Flags().Bool("rl", true, "Replace A tag links")
+	sendSubject = sendCmd.Flags().StringP("subject", "s", "", "Mail subject")
+
+	RootCmd.AddCommand(sendCmd)
+}
