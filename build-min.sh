ceaae4edfe61c27ad1cffc2d1e2be154da881aeb
Toomore Chiang <toomore0929@gmail.com>
2017-06-28 09:19:40 +0000

    Changed build mini

--- build-min.sh
+++ build-min.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 docker run -it --rm -v $(pwd)/mailbox_bin:/mailbox_bin toomore/mailbox:base    \
-    sh -c "go get -v ./cmd/...;
+    sh -c "go get -v ./...;
            cp /go/bin/* /mailbox_bin;"
 
-docker build -t toomore/mailbox:cmd ./
+docker build -t toomore/mailbox:cmd -f ./Dockerfile-min ./
 
 sudo rm -rf ./mailbox_bin
