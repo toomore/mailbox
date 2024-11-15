990cbcabc99f70a2a04f88e47b0169fce7288689
Toomore Chiang <toomore0929@gmail.com>
2024-11-15 18:36:56 +0800

    Changed to alpine 3.19
    Signed-off-by: Toomore Chiang <toomore0929@gmail.com>

--- build-base.sh
+++ build-base.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker pull alpine:3.20.2
-docker pull golang:1.22.2-alpine3.20
+docker pull alpine:3.19.4
+docker pull golang:1.22.2-alpine3.19
 docker build -t toomore/mailbox:base ./
--- dev-run-docker.sh
+++ dev-run-docker.sh
@@ -13,4 +13,4 @@ docker run -it --rm --link mailbox-mariadb:MARIADB                             \
            -e "mailbox_ses_sender=???"                                         \
            -e "mailbox_ses_replyto=???"                                        \
            -e "mailbox_web_site=???"                                           \
-           golang:1.22.2-alpine3.20 bash
+           golang:1.22.2-alpine3.19 bash
